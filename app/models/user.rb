a4d305311ef428d3640fcdd166d9b710180eeb55
t-nishi421 <69189773+t-nishi421@users.noreply.github.com>
2020-09-28 17:00:50 +0900

    deviseのpasswordのバリデーションを変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nick_name, presence: true, uniqueness: {case_sensitive: true}
+
+  # has_one :profile
+  # has_one :comment
+  # has_many :credit_cards
+  # has_many :destinations
+  # has_many :purchase_histories
+  # has_many :items
 end
