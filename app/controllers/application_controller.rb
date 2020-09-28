a0aa13c52cf5b7b95d51fb9eca4fa77e473f0ba7
t-nishi421 <69189773+t-nishi421@users.noreply.github.com>
2020-09-28 17:38:01 +0900

    usersテーブルに登録できるように編集

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,15 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth, if: :production?
 
+  before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nick_name])
+  end
+
   private
 
   def production?
