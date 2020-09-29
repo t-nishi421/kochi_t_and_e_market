56b8257ffbaad576d46cb68c1bdeea9a8bb8dccc
t-nishi421 <69189773+t-nishi421@users.noreply.github.com>
2020-09-29 15:04:13 +0900

    各モデルにアソシエーションを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,10 +6,10 @@ class User < ApplicationRecord
 
   validates :nick_name, presence: true, uniqueness: {case_sensitive: true}
 
-  # has_one :profile
+  has_one :profile
   # has_one :comment
   # has_many :credit_cards
-  # has_many :destinations
+  has_many :destinations
   # has_many :purchase_histories
   # has_many :items
 
