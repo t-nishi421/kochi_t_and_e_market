24fe19c2f79ffa9e0b7603247da3a68a58780272
t-nishi421 <69189773+t-nishi421@users.noreply.github.com>
2020-09-29 17:46:08 +0900

    新規登録ページを移動

--- app/controllers/signups_controller.rb
+++ app/controllers/signups_controller.rb
@@ -1,2 +1,25 @@
 class SignupsController < ApplicationController
+  def step1
+    @user = User.new
+  end
+
+  def step1_validates
+
+  end
+
+  def step2
+
+  end
+
+  def step2_validates
+
+  end
+
+  def step3
+
+  end
+
+  def create
+
+  end
 end
