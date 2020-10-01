d79f134daf7f3d47f13fac5981453d1d00c6c56b
t-nishi421 <69189773+t-nishi421@users.noreply.github.com>
2020-10-01 10:09:40 +0900

    各カラム名日本語かとバリデーション追加（途中）

--- app/models/destination.rb
+++ app/models/destination.rb
@@ -1,3 +1,20 @@
 class Destination < ApplicationRecord
   belongs_to :user, optional: true
+
+  validates :family_name, presence: true
+  validates :first_name, presence: true
+  validates :family_name_kana, presence: true,
+  format: {
+    with: /\A[\p{katakana}　ー－&&[^ -~｡-ﾟ]]+\z/,
+    message: "は全角カタカナのみで入力して下さい"
+  }
+  validates :first_name_kana, presence: true,
+  format: {
+    with: /\A[\p{katakana}　ー－&&[^ -~｡-ﾟ]]+\z/,
+    message: "は全角カタカナのみで入力して下さい"
+  }
+  validates :postal_code, presence: true, length: { is: 7 }, numericality: :only_integer
+  validates :prefecture, presence: true
+  validates :city, presence: true
+  validates :house_number, presence: true
 end
--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,3 +1,18 @@
 class Profile < ApplicationRecord
   belongs_to :user, optional: true
+
+  validates :family_name, presence: true
+  validates :first_name, presence: true
+  validates :family_name_kana, presence: true,
+    format: {
+    with: /\A[\p{katakana}　ー－&&[^ -~｡-ﾟ]]+\z/,
+    message: "は全角カタカナのみで入力して下さい"
+  }
+  validates :first_name_kana, presence: true,
+  format: {
+    with: /\A[\p{katakana}　ー－&&[^ -~｡-ﾟ]]+\z/,
+    message: "は全角カタカナのみで入力して下さい"
+  }
+  # validates :birthday
+
 end
