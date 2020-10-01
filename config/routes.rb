1edd728b11f0faae8283cfce11227fac35b374dc
Mitsuhiro-8 <mkn.full.buster@gmail.com>
2020-10-01 23:26:06 +0900

    add profiles_controller

--- config/routes.rb
+++ config/routes.rb
@@ -3,13 +3,14 @@ Rails.application.routes.draw do
   devise_for :users
   root 'items#index'
   
-  resources :users, only: [:show] do
+  resources :users, only: [:show, :edit, :update] do
     resources :credit_cards, only: [:new, :create, :edit, :update], as: :cards
     resources :destinations, only: [:new, :create, :edit, :update]
+    resources :profiles, only: [:edit, :update]
     collection do
-      get 'logout'
       get 'destination'
       get 'card'
+      get 'logout'
     end
   end
 
