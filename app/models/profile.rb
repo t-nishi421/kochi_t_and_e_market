0b1d52fdb085ebf8851b5d2f68eea1eda317797a
t-nishi421 <69189773+t-nishi421@users.noreply.github.com>
2020-10-01 10:23:00 +0900

    バリデーション追加

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,18 +1,26 @@
 class Profile < ApplicationRecord
   belongs_to :user, optional: true
 
-  validates :family_name, presence: true
-  validates :first_name, presence: true
+  validates :family_name, presence: true,
+    format: {
+      with: /\A[ぁ-んァ-ン一-龥]/,
+      message: "は全角で入力して下さい"
+    }
+  validates :first_name, presence: true,
+    format: {
+      with: /\A[ぁ-んァ-ン一-龥]/,
+      message: "は全角で入力して下さい"
+    }
   validates :family_name_kana, presence: true,
     format: {
     with: /\A[\p{katakana}　ー－&&[^ -~｡-ﾟ]]+\z/,
     message: "は全角カタカナのみで入力して下さい"
   }
   validates :first_name_kana, presence: true,
-  format: {
-    with: /\A[\p{katakana}　ー－&&[^ -~｡-ﾟ]]+\z/,
-    message: "は全角カタカナのみで入力して下さい"
-  }
+    format: {
+      with: /\A[\p{katakana}　ー－&&[^ -~｡-ﾟ]]+\z/,
+      message: "は全角カタカナのみで入力して下さい"
+    }
   # validates :birthday
 
 end
