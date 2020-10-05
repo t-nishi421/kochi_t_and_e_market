ee3c553de4b8fc5358cee359b177c21e5db351ef
t-nishi421 <69189773+t-nishi421@users.noreply.github.com>
2020-10-05 14:55:32 +0900

    画像拡大表示の不具合を修正

--- app/assets/javascripts/view_larger_image.js
+++ app/assets/javascripts/view_larger_image.js
@@ -6,6 +6,8 @@ $(function(){
   let area_a = 0; // rgbaのa
   let image_size = 0; // 拡大表示の画像サイズ
   let repeat_num = 20; // 各メソッドの繰り返し数
+  var now_event = false; // イベント発火中に他のイベント発火を防止する
+
 
   // *** メソッド ***
 
@@ -41,28 +43,37 @@ $(function(){
   // *** イベント処理 ***
 
   // 画像の出現イベント
+
   $('.SelectedImage').on('click', function(){
-    Area.css('display', 'block');
+    if(now_event == false) {
+      now_event = true;
+      Area.css('display', 'block');
       var appear = setInterval(() => {
         if(area_a < 0.4) {
           area_a = back_ground_display(area_a);
           image_size = view_image_size(image_size);
         } else {
           clearInterval(appear);
+          now_event = false;
         }
       }, 10);
+    }
   });
 
   // 画像が縮小するイベント
   $('.ViewImageArea').on('click', function(){
-    var disappear = setInterval(() => {
-      if(area_a > 0) {
-        area_a = back_ground_display_return(area_a);
-        image_size = view_image_size_return(image_size);
-      } else {
-        clearInterval(disappear);
-        Area.css('display', 'none');
-      }
-    }, 10);
+    if(now_event == false) {
+      now_event = true;
+      var disappear = setInterval(() => {
+        if(area_a > 0) {
+          area_a = back_ground_display_return(area_a);
+          image_size = view_image_size_return(image_size);
+        } else {
+          clearInterval(disappear);
+          Area.css('display', 'none');
+          now_event = false;
+        }
+      }, 10);
+    }
   });
 });
