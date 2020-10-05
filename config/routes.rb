efee7a39ad658a4fb851b1dacd4c990320840757
muuminmuumin <izgomakomatw3623@gmail.com>
2020-10-05 17:04:51 +0900

    ルーティング修正

--- config/routes.rb
+++ config/routes.rb
@@ -20,11 +20,10 @@ Rails.application.routes.draw do
     end
   end
 
-  resources :items, only: [:index, :show]
+  resources :items, only: [:index, :show, :new]
 
   root 'items#index'
 
-  resources :items, only: [:index, :new]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   
   resources :users, only: [:show, :edit, :update] do
