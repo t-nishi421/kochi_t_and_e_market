565762fba339a403c63d535e7a3a39980cbc9f29
t-nishi421 <69189773+t-nishi421@users.noreply.github.com>
2020-10-06 17:50:00 +0900

    画像アップロードの雛形作成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,11 +3,12 @@ class ItemsController < ApplicationController
   end
   
   def new
-    @item = Item.new();
+    @item = Item.new()
+    @item.item_images.new
   end
 
   def create
-    @item = Item.new(item_params);
+    @item = Item.new(item_params)
   end
 
   def show
@@ -17,6 +18,8 @@ class ItemsController < ApplicationController
 
   def item_params
     # 未実装 → category_idを追加予定
-    params.require(:item).permit(:name, :price, :introduction, :brand_id, :condition_id, :shipping_cost_id, :preparation_day_id, :prefecture_id)
+    params.require(:item).permit(:name, :price, :introduction, :brand_id, :condition_id,
+                                 :shipping_cost_id, :preparation_day_id, :prefecture_id,
+                                 item_images_attributes: [:image])
   end
 end
