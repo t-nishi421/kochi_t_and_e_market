579f56e357076b289dd99285ec055265986bc0ae
t-nishi421 <69189773+t-nishi421@users.noreply.github.com>
2020-10-06 13:53:21 +0900

    テーブルの修正、アクティブハッシュの選択タブ表示

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,11 +3,11 @@ class Item < ApplicationRecord
   # has_many :item_images
   # has_many :purchase_histories
   belongs_to :user
-  belongs_to :condition
-  belongs_to :shipping_cost
+  belongs_to_active_hash :condition
+  belongs_to_active_hash :shipping_cost
   belongs_to :category
   belongs_to :brand
-  belongs_to :preparation_day
-  belongs_to :prefecture
+  belongs_to_active_hash :preparation_day
+  belongs_to_active_hash :prefecture
 
 end
