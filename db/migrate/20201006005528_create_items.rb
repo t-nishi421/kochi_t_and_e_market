7c5661c89708ef5bd9409d16818f70c302eba173
t-nishi421 <69189773+t-nishi421@users.noreply.github.com>
2020-10-06 10:38:36 +0900

    Update 20201006005528_create_items.rb

--- db/migrate/20201006005528_create_items.rb
+++ db/migrate/20201006005528_create_items.rb
@@ -7,10 +7,10 @@ class CreateItems < ActiveRecord::Migration[6.0]
       t.string :trading_status,      null: false
       t.references :category,        null: false, foreign_key: true
       t.references :brand,                        foreign_key: true
-      t.integer :condition,       null: false
-      t.integer :shipping_cost,   null: false
-      t.integer :preparation_day, null: false
-      t.integer :prefecture, null: false
+      t.integer :condition,          null: false
+      t.integer :shipping_cost,      null: false
+      t.integer :preparation_day,    null: false
+      t.integer :prefecture,         null: false
       t.references :user,            null: false, foreign_key: true
 
       t.timestamps
