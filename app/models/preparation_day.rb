68778bedf3769cec4a8d891ca56c432ccd985cbc
t-nishi421 <69189773+t-nishi421@users.noreply.github.com>
2020-10-06 10:35:06 +0900

    誤字を修正

--- app/models/preparation_day.rb
+++ app/models/preparation_day.rb
@@ -2,6 +2,6 @@ class Preparation_day < ActiveHash::Base
   self.data = [
     {id: 1, period: '1~2日で発送'},
     {id: 2, period: '2~3日で発送'},
-    {id: 3, period: '4~7日で発送'},
+    {id: 3, period: '4~7日で発送'}
   ]
 end
