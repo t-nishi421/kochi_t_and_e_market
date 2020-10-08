a7dc8d6f3118c6d51c52399d89f42a0f7bf55e76
t-nishi421 <69189773+t-nishi421@users.noreply.github.com>
2020-10-08 11:15:00 +0900

    パスワードのバリデーションの不具合を修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,11 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
+  devise :confirmable, :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
   validates :nick_name, presence: true, uniqueness: {case_sensitive: true}
+  validates :password, confirmation: true
 
   has_one :profile
   # has_one :comment
