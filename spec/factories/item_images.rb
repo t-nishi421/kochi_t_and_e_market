9a38d3ea2b4f8c0ae8d4099ae7635a464b840c7d
t-nishi421 <69189773+t-nishi421@users.noreply.github.com>
2020-10-09 17:18:59 +0900

    画像テストの日雛形を作成

--- spec/factories/item_images.rb
+++ spec/factories/item_images.rb
@@ -1,5 +1,7 @@
 FactoryBot.define do
+
   factory :item_image do
-    
+    src  {"sample.png"}
+    item {Item.find_by_id(1)}
   end
 end
--- spec/models/item_image_spec.rb
+++ spec/models/item_image_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
+describe ItemImage do
+  before do
+    @item = FactoryBot.build(:item)
+    @item.item_images.new(FactoryBot.build(:item_image))
+  end
 
-RSpec.describe ItemImage, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '商品出品時の画像テスト' do
+
+    # 通ってないテスト
+    it "正常な入力値だと有効" do
+      expect(@item.item_images).to be_valid
+    end
+
+    # 通ってないテスト
+    it "画像のurlが未入力だとエラー" do
+      @item.item_images.src = ""
+      @item.item_images.valid?
+      expect(@item.item_images.errors[:src]).to include("を入力してください")
+    end
+  end
 end
