489a131ec8c0e0949d9f4f2cf91232ae3fd0d7cf
Mitsuhiro-8 <mkn.full.buster@gmail.com>
2020-10-14 11:56:50 +0900

    Update credit_cards_controller.rb

--- app/controllers/credit_cards_controller.rb
+++ app/controllers/credit_cards_controller.rb
@@ -26,20 +26,6 @@ class CreditCardsController < ApplicationController
       customer.cards.create(card: params[:payjp_token])
       redirect_to user_credit_cards_path(current_user), notice: "カードを追加しました"
     end
-    # Payjp.api_key = Rails.application.credentials.payjp[:secret_key]
-    # if current_user.credit_card.nil?
-    #   customer = Payjp::Customer.create(card: params[:payjp_token]) #顧客作成
-    #   card = current_user.build_credit_card(customer_token: customer.id, default: customer.default_card)
-    #   if card.save
-    #     redirect_to user_credit_cards_path(current_user), notice: "カードを登録しました"
-    #   else
-    #     redirect_to new_credit_cards_path
-    #   end
-    # else
-    #   customer = Payjp::Customer.retrieve(current_user.credit_card.customer_token) #既存の顧客情報を呼出
-    #   customer.cards.create(card: params[:payjp_token])
-    #   redirect_to user_credit_cards_path(current_user), notice: "カードを追加しました"
-    # end
   end
   
   def update # デフォルトカードの更新
@@ -52,24 +38,6 @@ class CreditCardsController < ApplicationController
     else
       render user_credit_cards_path(current_user), alert: "お支払い方法が変更できませんでした"
     end
-
-    # if params[:credit_card][:card] == current_user.credit_card.default
-      # redirect_to user_credit_cards_path(current_user), alert: "お支払い方法に変更ありません"
-
-    # @card = CreditCard.find_by(user_id: params[:user_id])
-    # if params[:credit_card][:card] == current_user.credit_card.default
-    #   redirect_to user_credit_cards_path(current_user), alert: "お支払い方法に変更ありません"
-    # else
-    #   Payjp.api_key = Rails.application.credentials.payjp[:secret_key]
-    #   customer = Payjp::Customer.retrieve(current_user.credit_card.customer_token)
-    #   customer.default_card = params[:credit_card][:card]
-    #   customer.save
-    #   if @card.update(default: customer.default_card)
-    #     redirect_to user_credit_cards_path(current_user), notice: "お支払い方法を変更しました"
-    #   else
-    #     redirect_to user_credit_cards_path(current_user), alert: "お支払い方法が変更できませんでした"
-    #   end
-    # end
   end
 
   def destroy # カード情報の削除とデフォルトカードの更新
