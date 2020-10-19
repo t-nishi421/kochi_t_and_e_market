fd57b597487f8576d3352cfb811ac3024d25a8ca
t-nishi421 <69189773+t-nishi421@users.noreply.github.com>
2020-10-19 14:41:02 +0900

    コメント投稿機能のテスト

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
-RSpec.describe Comment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Item do
+
+  before do
+    @comment = FactoryBot.build(:comment)
+  end
+
+  describe 'コメント投稿機能テスト' do
+    it "正常な入力値だと有効" do
+      expect(@comment).to be_valid
+    end
+
+    it "コメントが未入力だとエラー" do
+      @comment.comment = ""
+      @comment.valid?
+      expect(@comment.errors[:comment]).to include("を入力してください")
+    end
+  end
 end
