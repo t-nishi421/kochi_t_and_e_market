6b6078d718d2bc0fa3a988d9aa69d4ebad8e374f
t-nishi421 <69189773+t-nishi421@users.noreply.github.com>
2020-10-21 13:09:25 +0900

    トップページの画面縮小時に商品一覧がはみ出る問題を修正

--- app/assets/javascripts/resize_css.js
+++ app/assets/javascripts/resize_css.js
@@ -1,32 +1,31 @@
 $(document).on('turbolinks:load', ()=> {
 
-  // 【マイページ】サイドバーのリサイズ
-  $(window).on('load resize', function(){
-    const main = '.myPage__sidebar';
-    const pair = '.myPage__main';
-    const sub = '.myPage';
-    const initialSize = '300px';
-    const initialMargin = '30px 0 30px 5vw';
-    const pairMaxW = $(pair).css('max-width');
-    const pairMinW = $(pair).css('min-width');
-
-    var winW = $(window).width();
-    var devW = 1000;
-    var pairW = $(pair).width();
-    if (winW <= devW) {
-      //devWpx以下の時の処理
-      $(main).css('max-width', pairMaxW);
-      $(main).css('min-width', pairMinW);
-      $(main).css('margin', '30px 5vw');
-      $(sub).css('flex-wrap', 'wrap-reverse');
-    } else {
-      //devWpxより大きい時の処理
-      $(main).css('max-width', initialSize);
-      $(main).css('min-width', initialSize);
-      $(main).css('margin', initialMargin);
-      $(sub).css('flex-wrap', '');
-    }
-  });
+// 【マイページ】サイドバーのリサイズ
+$(window).on('load resize', function(){
+  const main = '.myPage__sidebar';
+  const pair = '.myPage__main';
+  const sub = '.myPage';
+  const initialSize = '300px';
+  const initialMargin = '30px 0 30px 5vw';
+  const pairMaxW = $(pair).css('max-width');
+  const pairMinW = $(pair).css('min-width');
+  
+  var winW = $(window).width();
+  var devW = 1000;
+  if (winW <= devW) {
+    //devWpx以下の時の処理
+    $(main).css('max-width', pairMaxW);
+    $(main).css('min-width', pairMinW);
+    $(main).css('margin', '30px 5vw');
+    $(sub).css('flex-wrap', 'wrap-reverse');
+  } else {
+    //devWpxより大きい時の処理
+    $(main).css('max-width', initialSize);
+    $(main).css('min-width', initialSize);
+    $(main).css('margin', initialMargin);
+    $(sub).css('flex-wrap', '');
+  }
+});
 
   // 【商品詳細】商品詳細欄のリサイズ
   $(window).on('load resize', function(){
@@ -34,7 +33,7 @@ $(document).on('turbolinks:load', ()=> {
     const pair = '.CommentForm__button';
     const sub = '.ItemProfile__status';
     const initialSize = '620px';
-
+  
     var winW = $(window).width();
     var devW = 700;
     var pairW = $(pair).width();
