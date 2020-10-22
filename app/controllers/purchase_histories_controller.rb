2484e6a7a0dc91e5fa6dc257703aedaed1eeba9e
t-nishi421 <69189773+t-nishi421@users.noreply.github.com>
2020-10-22 10:58:13 +0900

    並び順を商品の更新日を参照した降順に変更

--- app/controllers/purchase_histories_controller.rb
+++ app/controllers/purchase_histories_controller.rb
@@ -1,7 +1,7 @@
 class PurchaseHistoriesController < ApplicationController
 
   def index
-    @items = Item.includes(:item_images, :purchase_histories).where(purchase_histories: {user_id: current_user.id})
+    @items = Item.includes(:item_images, :purchase_histories).where(purchase_histories: {user_id: current_user.id}).order(updated_at: 'DESC')
   end
 
 end
